394e2c46883dd52d13abd584709929c787372423
TRJUNED5 <trjuned2@gmail.com>
2024-05-16 01:15:52 +0600

    "hide"

--- backend/database/index.js
+++ backend/database/index.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose'); //importing mongoose
 
 const { MONGODB_CONNECTION_STRING } = require('../config/index'); // importing mongo URL from config/index
 
-//const connectionString = "mongodb+srv://Juned505:warneverchange@mern-project.jeu5bgx.mongodb.net/Coin_Biance?retryWrites=true&w=majority&appName=Mern-PROJECT"
-// username = Juned505 password = warneverchange collection = Coin_Biance
-
 const dbConnect = async () => {
   //connecting database
   try {
